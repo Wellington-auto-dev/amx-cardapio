99394b0591a9df1da2eea52055fd0215038a1d1e
Wellington Silva <wellington.autodev96@gmail.com>
2026-05-08 23:37:17 -0300

    feat: clerk em portugues brasil

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { ptBR } from '@clerk/localizations'
 
 const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={publishableKey} afterSignInUrl="/operador">
+    <ClerkProvider publishableKey={publishableKey} localization={ptBR} afterSignInUrl="/operador">
       <App />
     </ClerkProvider>
   </StrictMode>,
